47120a394dfdc2babdb4f44c940a534117d98d37
Wagyourtail <wagyourtail@wagyourtail.xyz>
2025-03-30 02:03:29 -0500

    threadsafe

--- commons-kt-kt/src/commonMain/kotlin/xyz/wagyourtail/commonskt/properties/CombiningDelegate.kt
+++ commons-kt-kt/src/commonMain/kotlin/xyz/wagyourtail/commonskt/properties/CombiningDelegate.kt
@@ -1,16 +1,20 @@
 package xyz.wagyourtail.commonskt.properties
 
+import kotlinx.atomicfu.locks.SynchronizedObject
+import kotlinx.atomicfu.locks.synchronized
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class CombiningDelegate<T>(private var value: T, val combiner: (T, T) -> T): ReadWriteProperty<Any?, T> {
+class CombiningDelegate<T>(private var value: T, val combiner: (T, T) -> T): SynchronizedObject(), ReadWriteProperty<Any?, T> {
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): T {
         return value
     }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-        this.value = combiner(this.value, value)
+        synchronized(this) {
+            this.value = combiner(this.value, value)
+        }
     }
 
 }
