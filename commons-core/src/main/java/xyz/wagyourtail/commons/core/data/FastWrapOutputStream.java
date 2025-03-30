a9ae2bb02ca87d2909d6991f96b632d9703f0486
Wagyourtail <wagyourtail@wagyourtail.xyz>
2025-03-29 22:26:27 -0500

    make fast wrap safer

--- commons-core/src/main/java/xyz/wagyourtail/commons/core/data/FastWrapOutputStream.java
+++ commons-core/src/main/java/xyz/wagyourtail/commons/core/data/FastWrapOutputStream.java
@@ -1,14 +1,25 @@
 package xyz.wagyourtail.commons.core.data;
 
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.nio.channels.SeekableByteChannel;
 
 public class FastWrapOutputStream extends ByteArrayOutputStream {
 
+    @Override
+    public synchronized void reset() {
+        throw new UnsupportedOperationException();
+    }
+
     @SuppressWarnings("resource")
     public SeekableByteChannel wrap() {
         return new SeekableInMemoryByteChannel(this.buf).truncate(this.count);
+    }
+
+    public InputStream asInputStream() {
+        return new ByteArrayInputStream(this.buf, 0, this.count);
     }
 
 }
