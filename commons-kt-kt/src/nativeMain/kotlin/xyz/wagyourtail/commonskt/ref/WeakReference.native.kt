489578d214e86547c2a3ed8000dee12ef1e12fe2
Wagyourtail <wagyourtail@wagyourtail.xyz>
2026-08-30 05:42:06 -0500

    fix build

--- commons-kt-kt/src/nativeMain/kotlin/xyz/wagyourtail/commonskt/ref/WeakReference.native.kt
+++ commons-kt-kt/src/nativeMain/kotlin/xyz/wagyourtail/commonskt/ref/WeakReference.native.kt
@@ -3,7 +3,7 @@ package xyz.wagyourtail.commonskt.ref
 import kotlin.experimental.ExperimentalNativeApi
 
 @OptIn(ExperimentalNativeApi::class)
-actual class WeakReference<T>(private val ref: kotlin.native.ref.WeakReference<T>) {
+actual class WeakReference<T: Any>(private val ref: kotlin.native.ref.WeakReference<T>) {
 
     actual constructor(value: T) : this(kotlin.native.ref.WeakReference(value))
 
